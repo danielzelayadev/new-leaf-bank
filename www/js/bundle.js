91af128e931503f71425e2340d04ffd7cc0d76c4
wupa9 <wupaz9@gmail.com>
2016-04-12 22:42:34 -0600

    Account creation

--- www/js/bundle.js
+++ www/js/bundle.js
@@ -49,40 +49,35 @@
   }
 
 })();
-( function(){
+( function() {
 
 	"use strict";
 
 	angular.module('ACBank')
 		.controller('AccountsController', AccountsController);
 
-	AccountsController.$inject = [ '$scope' ];
+	AccountsController.$inject = [ '$scope', '$ionicModal' ];
 
-	function AccountsController ($scope) {
+	function AccountsController ($scope, $ionicModal) {
 		var vm = this;
 
-		vm.accounts = 
-		[ 
-			{ 
-				name: 'Personal',
-				bells: 100200,
-				portion: 20
-			},
-			{ 
-				name: 'Town',
-				bells: 500890,
-				portion: 40
-			},
-			{ 
-				name: 'House',
-				bells: 300000,
-				portion: 40
-			}
-		];
-		vm.accountNameTbs = [ false, false, false ];
+		vm.account = {};
+		vm.accounts = [];
+		vm.accountNameTbs = [];
+		vm.addAccountClicked = addAccountClicked;
 		vm.removeAccountClicked = removeAccountClicked;
 		vm.reorder = reorder;
 		vm.toggleAccountNameTb = toggleAccountNameTb;
+		vm.closeModal = closeModal;
+		vm.confirmAccountCreation = confirmAccountCreation;
+
+		$scope.vm = vm;
+
+		function addAccountClicked () {
+			
+			$scope.modal.show();
+
+		}
 
 		function removeAccountClicked ($index) {
 			if (confirm("Are you sure?")) {
@@ -100,6 +95,41 @@
 		function toggleAccountNameTb ($index) {
 			vm.accountNameTbs[$index] = !vm.accountNameTbs[$index];
 		}
+
+		function closeModal () {
+			$scope.modal.hide();
+			$scope.account = {};
+		}
+
+		function confirmAccountCreation () {
+			closeModal();
+			addAccount(vm.account);
+		}
+
+		function addAccount (account) {
+			if (account.name === undefined || account.name.length <= 0) account.name = "Account " + vm.accounts.length;
+
+			if (!isNumber(account.bells) || account.bells < 0) account.bells = 0;
+
+			if (!isNumber(account.portion) || account.portion < 0 || account.portion > 100) account.portion = 0;
+			
+			vm.accounts.push(account);
+
+			vm.account = {};
+
+		}
+
+		function isNumber(n) {
+		  return !isNaN(parseFloat(n)) && isFinite(n);
+		}
+
+		$ionicModal.fromTemplateUrl('../templates/Accounts/add-account-modal.html', {
+		    scope: $scope,
+		    animation: 'slide-in-up'
+		  }).then(function(modal) {
+		    $scope.modal = modal;
+		 });
+
 	}
 
 })();
