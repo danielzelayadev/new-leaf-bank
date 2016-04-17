e826983d90ef7b4c1e5aa694418f60e87fb95a45
wupa9 <wupaz9@gmail.com>
2016-04-17 13:11:11 -0600

    Add confirm popup to delete accounts

--- app/components/Accounts/accounts.controller.js
+++ app/components/Accounts/accounts.controller.js
@@ -5,9 +5,9 @@
 	angular.module('ACBank')
 		.controller('AccountsController', AccountsController);
 
-	AccountsController.$inject = [ '$scope', '$ionicModal' ];
+	AccountsController.$inject = [ '$scope', '$ionicModal', '$ionicPopup' ];
 
-	function AccountsController ($scope, $ionicModal) {
+	function AccountsController ($scope, $ionicModal, $ionicPopup) {
 		var vm = this;
 
 		vm.options = [
@@ -27,8 +27,21 @@
 				clicked: function () { $scope.depositModal.show(); }
 			}
 		];
+		vm.deleteAccount = deleteAccount;
 
 		$scope.accounts = [];
+
+		function deleteAccount ($index, name) {
+		    let confirmPopup = $ionicPopup.confirm({
+		    	title: 'Delete Account',
+		    	template: 'Are you sure you want to delete ' + name + '?'
+		   	});
+
+		   	confirmPopup.then( function (res) {
+			    if(res)
+			        $scope.accounts.splice($index, 1);
+		   });
+		};
 
 		$ionicModal.fromTemplateUrl('../templates/NL-Modals/Add Account/add-account-modal.html', {
     		scope: $scope,
